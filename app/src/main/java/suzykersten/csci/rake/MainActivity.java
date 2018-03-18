b50d4f5b4e75ab3aeb79c88393ec863001e99d6f
Harrison Welch <hwelch1@my.apsu.edu>
2018-03-17 19:27:45 -0500

    placed button to go to rep activity; built a basic activty for reps

--- app/src/main/java/suzykersten/csci/rake/MainActivity.java
+++ app/src/main/java/suzykersten/csci/rake/MainActivity.java
@@ -1,7 +1,9 @@
 package suzykersten.csci.rake;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button_reps).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Transfer to Rep Activity for listing reps
+                Intent intent = new Intent(getApplicationContext(), RepresentativeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/suzykersten/csci/rake/RepresentativeActivity.java
@@ -0,0 +1,14 @@
+package suzykersten.csci.rake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RepresentativeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_representative);
+
+    }
+}
