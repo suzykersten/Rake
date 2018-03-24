60fbfc90fdd2969c1b45b04d961e63a01ef57a51
Suzy Kersten <suzykersten72@outlook.com>
2018-03-24 10:14:32 -0500

    enabled getting basic bill data

--- app/src/main/java/suzykersten/csci/rake/BillActivity.java
+++ app/src/main/java/suzykersten/csci/rake/BillActivity.java
@@ -2,6 +2,16 @@ package suzykersten.csci.rake;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class BillActivity extends Activity {
 
@@ -9,5 +19,36 @@ public class BillActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bill);
+
+        findViewById(R.id.button_getBill).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getBillData();
+            }
+        });
+    }
+
+
+    private void getBillData(){
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+        try {
+            TextView tv = findViewById(R.id.textView_billOutput);
+
+            URL url = new URL("https://www.govinfo.gov/content/pkg/BILLS-115sres99ats/xml/BILLS-115sres99ats.xml");
+            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
+            String line;
+
+            while ( (line = br.readLine()) != null){
+                tv.setText(tv.getText() +"\n" + line);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+
     }
 }
