029d842bc024bdd418b71d89b8fc289634b57ebb
Suzy Kersten <suzykersten72@outlook.com>
2018-04-24 20:11:46 -0500

    move the downloading of data to a background thread

--- app/src/main/java/suzykersten/csci/rake/BillActivity.java
+++ app/src/main/java/suzykersten/csci/rake/BillActivity.java
@@ -1,6 +1,8 @@
 package suzykersten.csci.rake;
 
 import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
@@ -51,29 +53,59 @@ public class BillActivity extends Activity {
         });
     }
 
-    private String readStream(InputStream in){
-        Scanner s = new Scanner(in).useDelimiter("\\A");
-        return s.hasNext() ? s.next() : "";
 
-
-    }
 
 
     private void getBillData(){
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 
-        StrictMode.setThreadPolicy(policy);
 
+
+
+        ListView listView = findViewById(R.id.listView_bills);
+        DownloadBills dlBills = new DownloadBills("https://sskersten.bitbucket.io/json/hr.json", listView);
+
+        dlBills.execute();
+
+        //((TextView) findViewById(R.id.textView_billOutput)).setText(result);
+    }
+
+
+}
+
+
+class DownloadBills extends AsyncTask<String, Integer, Bill[]>{
+    private String stringURL;
+    private ListView listView;
+
+    public DownloadBills(String stringURL, ListView listView) {
+        this.stringURL = stringURL;
+        this.listView = listView;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    private String readStream(InputStream in){
+        Scanner s = new Scanner(in).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
+
+    @Override
+    protected Bill[] doInBackground(String... strings) {
         String result = "";
 
+        //make a url
         URL url = null;
         try {
-            url = new URL("https://sskersten.bitbucket.io/json/hr.json");
+            url = new URL(stringURL);
         } catch (MalformedURLException e) {
             Log.e("ReadBill", "Bad URL given.");
             e.printStackTrace();
         }
 
+        //connect to the internet
         HttpURLConnection urlConnection = null;
         try {
             urlConnection = (HttpURLConnection) url.openConnection();
@@ -82,6 +114,7 @@ public class BillActivity extends Activity {
             e.printStackTrace();
         }
 
+        //read in data from the internet
         try {
             InputStream in = new BufferedInputStream(urlConnection.getInputStream());
             result = readStream(in);
@@ -92,89 +125,22 @@ public class BillActivity extends Activity {
             urlConnection.disconnect();
         }
 
+        //Log.e("result", result);
+
         //convert gotten json to Bills objects
         Gson gson = new Gson();
-        Bill[] bills = gson.fromJson(result, Bill[].class);
-
-
-        ListView listView = findViewById(R.id.listView_bills);
-        listView.setAdapter(new ArrayAdapter<>(getApplicationContext(),android.R.layout.simple_list_item_1, bills));
-
-        ((TextView) findViewById(R.id.textView_billOutput)).setText(result);
-
-
-    /*
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new BillActivity.JsonRepResListener(), new BillActivity.JsonRepResErrListener())
-        {
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                HashMap<String, String> headers = new HashMap<String, String>();
-                //headers.put("Content-Type", "application/json");
-                headers.put("accept", "*");
-                //headers.put("Content-Type", "application/json");
-                return headers;
-            }
-        }; */
-        //jsonObjectRequest.setTag(TAG_BILL_ACT);
-//        requestQueue.add(jsonObjectRequest);
-        //RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
-       // requestQueue.add(jsonObjectRequest);
-
-/*
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-
-        StrictMode.setThreadPolicy(policy);
-        try {
-            TextView tv = findViewById(R.id.textView_billOutput);
-
-            URL url = new URL("https://www.govinfo.gov/content/pkg/BILLS-115sres99ats/xml/BILLS-115sres99ats.xml");
-            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-            String line;
-
-            while ( (line = br.readLine()) != null){
-                tv.setText(tv.getText() +"\n" + line);
-            }
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-        */
-
-
+        return gson.fromJson(result, Bill[].class);
     }
 
-
-    private class JsonRepResListener implements Response.Listener<JSONObject>{
-        @Override
-        public void onResponse(JSONObject response) {
-            Log.i(TAG_BILL_ACT, "onResponse, response = " + response);
-            ((TextView) findViewById(R.id.textView_billOutput)).setText(response.toString());
-
-            // fill a vector with officials
-            try {
-                JSONArray jsonArray = response.getJSONArray("title");
-//                Vector<JSONObject> vecOfficials = new Vector<>();
-                Vector<String> vecOfficialsNames = new Vector<>();
-                for (int i = 0; i < jsonArray.length(); i++){
-//                    vecOfficials.add(jsonArray.getJSONObject(i));
-                    Log.i(TAG_BILL_ACT, "jsonArray.getJSONObject(i).get(\"title\").toString() = " + jsonArray.getJSONObject(i).get("title").toString());
-                    vecOfficialsNames.add(jsonArray.getJSONObject(i).get("name").toString());
-                }
-                ListView listView = findViewById(R.id.listView_bills);
-                listView.setAdapter(new ArrayAdapter<>(getApplicationContext(),android.R.layout.simple_list_item_1, vecOfficialsNames));
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-
-        }
+    //update the layout with the requested set of bills
+    @Override
+    protected void onPostExecute(Bill[] bills) {
+        listView.setAdapter(new ArrayAdapter<>(listView.getContext(),android.R.layout.simple_list_item_1, bills));
     }
 
-    private class JsonRepResErrListener implements Response.ErrorListener {
-
-        @Override
-        public void onErrorResponse(VolleyError error) {
-            Log.i(TAG_BILL_ACT, "onErrorResponse, error = " + error);
-            ((TextView) findViewById(R.id.textView_billOutput)).setText("Address not found. Sorry!");
-        }
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
     }
 }
+
